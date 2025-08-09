bb497c9ed504500217fcc6b22f53327817404ba0
Brian_Kim <briankimdw@gmail.com>
2025-08-09 12:55:29 -0400

    repo: keep only p6-route directory in repo

--- APT/APT 1/Gravity.java
+++
@@ -1,8 +0,0 @@
-public class Gravity {
-    public double falling(double time, double velo){
-      double displacement = 0;
-
-      displacement = (velo*time) + (.5*9.8*time*time);
-      return displacement;
-    }
-}
--- APT/APT 2/SandwichBar.java
+++
@@ -1,6 +0,0 @@
-public class SandwichBar {
-    public int whichOrder(String[] available, String[] orders){
-       // fill in code here
-       return 0; 
-    }
- }
